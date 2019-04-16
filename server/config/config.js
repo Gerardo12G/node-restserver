db89003172cb6ec21ca92516adfaf4c62c5f6638
adolfo Garcia <cofi_monadolf@outlook.com>
2019-04-16 13:01:06 -0500

    rest server-usuarios

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,12 @@
 process.env.PORT = process.env.PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://gAdolfo:plUbN3xUZgdHU5zY@cluster0-wcqc3.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
